4c9ea7c57f75fad34935934e7a7afe695e480adc
Simon F <github@19r.de>
2017-04-05 14:16:57 +0200

    v2.0.3α
    Neue Funktion: .NET Framework 3.5 de/installieren

--- Windows-Server-Installtool/scripts/servermanager.ps1
+++ Windows-Server-Installtool/scripts/servermanager.ps1
@@ -7,7 +7,7 @@ cls
 ### Startbildschirm ###
     function startbildschirm {
         Write-Host "╔═══════════════════════════════════════════════════════════════════════════════╗"
-        Write-Host "║ Windows Server-Manager Tool v2.0.2α                                           ║"
+        Write-Host "║ Windows Server-Manager Tool v2.0.3α                                           ║"
         Write-Host "║                                                                               ║"
         Write-Host "╚═══════════════════════════════════════════════════════════════════════════════╝"
     }
@@ -51,12 +51,94 @@ function menueauswahl {
                 '5' {entwicklung}
                 '6' {entwicklung}
                 '7' {entwicklung}
-                '8' {entwicklung}
+                '8' {netframework35-menu}
                 '9' {entwicklung}
                 '10' {entwicklung}
                 'x' {wsitool}
             } pause }
         until ($input -eq 'x')
+}
+
+### .NET Framework 3.5 ###
+function netframework35-menu {
+    do {
+        cls
+        startbildschirm
+            Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
+            Write-Host "   ║ .NET Framework 3.5                                                            ║"
+            Write-Host "   ╠══════════════════════                                                         ║"
+            Write-Host "   ║                                                                               ║"
+            Write-Host "   ║ Möchten Sie .NET Framework installieren oder deinstallieren?                  ║"
+            Write-Host "   ║                                                                               ║"
+            Write-Host "   ║ [ 1 ] Installieren                   ║ [ 2 ] Deinstallieren                   ║"
+            Write-Host "   ║                                      ║                                        ║"
+            Write-Host "   ╠══════════════════════════════════════╩════════════════════════════════════════╣"
+            Write-Host "   ║ [ X ] Zurück zum Hauptmenü                                                    ║"
+            Write-Host "   ║                                                                               ║"
+            Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
+            Write-Host ""
+            $input = Read-Host "Bitte wählen Sie"
+
+            switch ($input) {
+                '1' {netframework35-install}
+                '2' {netframework35-uninstall}
+                'x' {menueauswahl} # Zurück ins Hauptmenü #
+            } pause }
+        until ($input -eq 'x')
+}
+
+### .NET Framework 3.5 installieren ###
+function netframework35-install {
+    cls
+    startbildschirm
+        Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
+        Write-Host "   ║ .NET Framework 3.5                                                            ║"
+        Write-Host "   ╠══════════════════════                                                         ║"
+        Write-Host "   ║                                                                               ║"
+        Write-Host "   ║ .NET Framework 3.5 wird installiert...                                        ║"
+        Write-Host "   ║                                                                               ║"
+        Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
+        Write-Host ""
+        Start-Sleep -Milliseconds 1500
+        Install-WindowsFeature Net-Framework-Core -source \\network\share\sxs | Out-Null
+        cls
+        startbildschirm
+            Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
+            Write-Host "   ║ .NET Framework 3.5                                                            ║"
+            Write-Host "   ╠══════════════════════                                                         ║"
+            Write-Host "   ║                                                                               ║"
+            Write-Host "   ║ .NET Framework 3.5 wurde erfolgreich installiert...                           ║"
+            Write-Host "   ║                                                                               ║"
+            Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
+            Start-Sleep -Milliseconds 3000
+    menueauswahl
+}
+
+### .NET Framework 3.5 deinstallieren ###
+function netframework35-uninstall {
+    cls
+    startbildschirm
+        Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
+        Write-Host "   ║ .NET Framework 3.5                                                            ║"
+        Write-Host "   ╠══════════════════════                                                         ║"
+        Write-Host "   ║                                                                               ║"
+        Write-Host "   ║ .NET Framework 3.5 wird deinstalliert...                                      ║"
+        Write-Host "   ║                                                                               ║"
+        Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
+        Write-Host ""
+        Start-Sleep -Milliseconds 1500
+        Uninstall-WindowsFeature Net-Framework-Core | Out-Null
+        cls
+        startbildschirm
+            Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
+            Write-Host "   ║ .NET Framework 3.5                                                            ║"
+            Write-Host "   ╠══════════════════════                                                         ║"
+            Write-Host "   ║                                                                               ║"
+            Write-Host "   ║ .NET Framework 3.5 wurde erfolgreich deinstalliert...                         ║"
+            Write-Host "   ║                                                                               ║"
+            Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
+            Start-Sleep -Milliseconds 3000
+    menueauswahl
 }
 
 ### Root-Verzeichnis ermitteln, zum öffnen des Programmcodes ###
@@ -136,6 +218,4 @@ function neustart {
 }
 
 ### Start ###
-startbildschirm
-menue
 menueauswahl
