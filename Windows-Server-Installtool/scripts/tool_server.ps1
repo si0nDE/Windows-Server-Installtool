32620f4d9c8111a890b98d2c68ef6a0e57d47aee
Simon F <github@19r.de>
2017-05-22 13:13:45 +0200

    v1.7.0
    - Hinzugefügt: Arbeitsgruppen und Domänenbeitritt in eigenes Script.
    - Hinzugefügt: Fehlermeldung, wenn Arbeitsgruppen oder Domänen nicht
    beigetreten werden kann.
    - Fehler behoben: Fehler-Ausgaben von PowerShell im Arbeitgruppen oder
    Domänenbeitritt werden unterdrückt
    - Hinzugefügt: Fehlermeldung, wenn Updates fehlschlagen

--- Windows-Server-Installtool/scripts/tool_server.ps1
+++ Windows-Server-Installtool/scripts/tool_server.ps1
@@ -114,94 +114,33 @@ function netzwerktool {
     & $netzwerk_fullscriptpath
 }
 
+
+
 ### Arbeitsgruppe/Domäne beitreten - Menü ###
 function workgroupdomaintool {
-    do {
-        cls
-        startbildschirm
-            Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
-            Write-Host "   ║ Arbeitsgruppe/Domäne beitreten                                                ║"
-            Write-Host "   ╠══════════════════════════════════                                             ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ║ Möchten Sie einer Arbeitsgruppe oder einer Domäne beitreten?                  ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ║ [ 1 ] Arbeitsgruppe                  ║ [ 2 ] Domäne                           ║"
-            Write-Host "   ║                                      ║                                        ║"
-            Write-Host "   ╠══════════════════════════════════════╩════════════════════════════════════════╣"
-            Write-Host "   ║ [ X ] Zurück zum Hauptmenü                                                    ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
-            Write-Host ""
-            $input = Read-Host "Bitte wählen Sie"
-
-            switch ($input) {
-                '1' {arbeitsgruppe_beitreten}
-                '2' {domaene_beitreten}
-                'x' {menueauswahl} # Zurück ins Hauptmenü #
-            } pause }
-        until ($input -eq 'x')
-}
-
-### Arbeitsgruppe beitreten ###
-function arbeitsgruppe_beitreten {
     cls
-    startbildschirm
-        Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
-        Write-Host "   ║ Arbeitsgruppe beitreten                                                       ║"
-        Write-Host "   ╠═══════════════════════════                                                    ║"
-        Write-Host "   ║                                                                               ║"
-        Write-Host "   ║ Geben Sie die Arbeitsgruppe ein, der Sie beitreten möchten...                 ║"
-        Write-Host "   ║                                                                               ║"
-        Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
-        Write-Host ""
-        $neuearbeitsgruppe = Read-Host "Neue Arbeitsgruppe"
-        Start-Sleep -Milliseconds 1500
-        $computerinfo.JoinDomainOrWorkgroup("$neuearbeitsgruppe") | Out-Null
-        cls
-        startbildschirm
-            Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
-            Write-Host "   ║ Arbeitsgruppe beitreten                                                       ║"
-            Write-Host "   ╠═══════════════════════════                                                    ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ║ Die Arbeitsgruppe wurde erfolgreich geändert.                                 ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ║ Ein Neustart ist erforderlich!                                                ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
-            Start-Sleep -Milliseconds 3000
-    menueauswahl
+        $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        $princ = New-Object System.Security.Principal.WindowsPrincipal($identity)
+        if(!$princ.IsInRole( `
+            [System.Security.Principal.WindowsBuiltInRole]::Administrator))
+            {
+                $powershell = [System.Diagnostics.Process]::GetCurrentProcess()
+                $psi = New-Object System.Diagnostics.ProcessStartInfo $powerShell.Path
+                $script = "$installpath\script_domainworkgroup.ps1"
+                $prm = $script
+                    foreach($a in $args) {
+                        $prm += ' ' + $a
+                    }
+                $psi.Arguments = $prm
+                $psi.Verb = "runas"
+                [System.Diagnostics.Process]::Start($psi) | Out-Null
+                return;
+            }
+    ### Falls Adminrechte nicht erfordert werden können, ###
+    ### soll das Script trotzdem ausgeführt werden.      ###
+    & "$installpath\script_domainworkgroup.ps1"
 }
 
-### Domäne beitreten ###
-function domaene_beitreten {
-    cls
-    startbildschirm
-        Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
-        Write-Host "   ║ Domäne beitreten                                                              ║"
-        Write-Host "   ╠════════════════════                                                           ║"
-        Write-Host "   ║                                                                               ║"
-        Write-Host "   ║ Geben Sie die Domäne ein, der Sie beitreten möchten...                        ║"
-        Write-Host "   ║                                                                               ║"
-        Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
-        Write-Host ""
-        $neuedomaene = Read-Host "Neue Domäne"
-        $benutzername = Read-Host "Benutzername"
-        Start-Sleep -Milliseconds 1500
-        Add-Computer -DomainName $neuedomaene -Credential $neuedomaene\$benutzername
-        cls
-        startbildschirm
-            Write-Host "   ╔═══════════════════════════════════════════════════════════════════════════════╗"
-            Write-Host "   ║ Domäne beitreten                                                              ║"
-            Write-Host "   ╠════════════════════                                                           ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ║ Die Domäne wurde erfolgreich geändert.                                        ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ║ Ein Neustart ist erforderlich!                                                ║"
-            Write-Host "   ║                                                                               ║"
-            Write-Host "   ╚═══════════════════════════════════════════════════════════════════════════════╝"
-            Start-Sleep -Milliseconds 3000
-    menueauswahl
-}
 
 ### Verstärkte Sicherheitskonfiguration für IE - Menü ###
 function iexplorer_sicherheit {
